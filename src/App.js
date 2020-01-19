eb374118b7abb92f278aed1b71714ae91bc983d1
Miroslav <miki.skela90@gmail.com>
2020-01-19 06:47:42 -0600

    initial routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import {Route} from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
 
 import './App.css';
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,8 +1,11 @@
 import React from "react";
+import {withRouter} from 'react-router-dom';
 import "./menu-item.styles.scss";
-export default function MenyItem({ title, imageUrl, size }) {
+function MenyItem({ title, imageUrl, size, history, linkUrl, match }) {
+    console.log(history)
+    console.log(match)
   return (
-    <div className={`${size} menu-item`}>
+    <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
       <div
         style={{ backgroundImage: `url(${imageUrl})` }}
         className="background-image"
@@ -14,3 +17,5 @@ export default function MenyItem({ title, imageUrl, size }) {
     </div>
   );
 }
+
+export default withRouter(MenyItem)
